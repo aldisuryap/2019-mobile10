4f309a8bc66a0e2eb5988c03d332f4d0e5cb0bd3
aldisuryap <appleimport123@gmail.com>
2019-11-01 14:23:57 +0700

    View Model Lanjut

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/MainActivity.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/MainActivity.java
@@ -1,8 +1,12 @@
 package id.ac.polinema.skorbasket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+
+import id.ac.polinema.skorbasket.viewmodels.SharedScore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,5 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
 }
